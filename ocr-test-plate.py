d6eb213e18eedb2f3be72b38dce555a3bc4e0e96
ferauche <thiago.ferauche@gmail.com>
2021-10-19 19:13:49 +0100

    semana nacional de ciencia e tecnologia da Unisantos

---
+++ ocr-test-plate.py
@@ -0,0 +1,20 @@
+import cv2
+import pytesseract
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+
+time.sleep(1.2)
+
+camera.capture(rawCapture, format="bgr")
+image = cv2.cvtColor(rawCapture.array, cv2.COLOR_BGR2GRAY) 
+
+print("Lendo...")
+opt = "-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+opt += " --oem 3 --psm 6"
+
+text = pytesseract.image_to_string(image, config = opt)
+print(text.replace(" ",""))
